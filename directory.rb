1458be68603fda3926a7ec9b6ac57564ff543ab8
TomGroombridge <groombridgetom@gmail.com>
2013-11-12 16:50:04 +0000

    simplified

--- directory.rb
+++ directory.rb
@@ -1,11 +1,19 @@
 
-student_count = 5
 # first we print the names of the students
+students = [
+"Tom",
+"J.B",
+"Lara", 
+"James",
+"Kennedy",
+"Tom",
+"Nicki"
+]
+#and then print them
 puts "The students of my cohort at Makers Academy"
-puts "Tom"
-puts "J.B"
-puts "Lara"
-puts "James"
-puts "Kennedy"
+puts "--------------"
+students.each do |student|
+puts student
+end
 #then we print the total
-puts "overall, we have #{student_count} great students"
+puts "overall, we have #{students.length} great students"
