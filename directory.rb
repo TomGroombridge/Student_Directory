60783e18be11d8c6ecbbc013392d6ac013bef9d6
TomGroombridge <groombridgetom@gmail.com>
2013-11-20 09:09:13 +0000

    interactive_menu

--- directory.rb
+++ directory.rb
@@ -1,16 +1,6 @@
 
 # first we print the names of the students
-students = [
-{:name => "Tom", :cohort => :november},
-{:name => "J.B", :cohort => :november},
-{:name => "Lara", :cohort => :november}, 
-{:name => "James", :cohort => :november},
-{:name => "Kennedy", :cohort => :november},
-{:name => "Tom", :cohort => :november},
-{:name => "Nicki", :cohort => :november},
-{:name => "Chris", :cohort => :november},          
-{:name => "Kumi", :cohort => :november}, 
-           ]
+students =  {:name => "", :age => "", :cohort => :november}
 #and then print them
 
 def print_header
@@ -20,43 +10,69 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]}"
+    puts "age: #{student[:age]}"
+    puts "(#{student[:cohort]} cohort)"
+    puts ""
   end
 end
 
 
 def input_students
-  puts "please enter the name of students"
+  puts "please enter your name and hit enter"
+  puts "then enter your age"
+  puts "FOR EXAMPLE \"Tom Groombridge \"\(Enter\)"
+  puts "\"19\"\(Enter\)"
   puts "hit return twice to finish"
 
-  #create a empty array
-  students = []
-
-  #get the first name 
+  students = [] 
   name = gets.chomp
+  age = gets.chomp
 
-  #while the name is not empty repeat code
   while !name.empty? do 
-
-  #add the student hash to the array
-  students << {:name => name, :cohort => :Novemeber}
-
-  puts "now we have #{students.length} students"
-
-  #get the next name 
+  students << {:name => name, :age => age, :cohort => :Novemeber}
+  puts "now we have #{students.length} students" 
   name = gets.chomp
-end
-    #reuturn the array of students
+  age = gets.chomp
+  end
     students
 end
-
 
 def print_footer (students)
   puts "overall, we have #{students.length} great students"
 end
- #nothing happnes until we assign the variables
+
+def interactive_menu
+  students = []
+  loop do 
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+  selection = gets.chomp
+  
+  case selection
+  when "1"
+  students = input_students
+  when "2"
+  print_header
+  print(students)
+  print_footer(students)
+  when "9"
+  exit  
+  else
+  "Sorry I do not understand"
+  end
+  
+end
+
+
  
- students = input_students
- print_header
- print(students)
- print_footer(students)
+end
+ 
+ interactive_menu
+ #students = input_students
+ #print_header
+ #print(students)
+ #print_footer(students)
+
+
