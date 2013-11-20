9b5645b3c8c0fb0d92baee0451e1d538587423d0
TomGroombridge <groombridgetom@gmail.com>
2013-11-20 11:18:59 +0000

    refactor methods

--- directory.rb
+++ directory.rb
@@ -1,14 +1,12 @@
-
-# first we print the names of the students
-students =  {:name => "", :age => "", :cohort => :november}
-#and then print them
+@students =  []
 
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "--------------"
 end
 
-def print(students)
+
+def print (students)
   students.each do |student|
     puts "#{student[:name]}"
     puts "age: #{student[:age]}"
@@ -17,14 +15,16 @@ def print(students)
   end
 end
 
-
-def input_students
+def print_input_students
   puts "please enter your name and hit enter"
   puts "then enter your age"
   puts "FOR EXAMPLE \"Tom Groombridge \"\(Enter\)"
   puts "\"19\"\(Enter\)"
   puts "hit return twice to finish"
+end
 
+
+def input_students
   students = [] 
   name = gets.chomp
   age = gets.chomp
@@ -35,44 +35,52 @@ def input_students
   name = gets.chomp
   age = gets.chomp
   end
-    students
+  @students = students
 end
+
 
 def print_footer (students)
-  puts "overall, we have #{students.length} great students"
-end
+  puts"overall, we have #{students.length} great students"
+end 
 
-def interactive_menu
-  students = []
-  loop do 
+
+def menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit"
+end 
+
+
+def show_students 
+ print_header
+ print(@students)
+ print_footer (@students)
+end 
+
+
+def process
   selection = gets.chomp
-  
   case selection
   when "1"
-  students = input_students
+  print_input_students
+  input_students
   when "2"
-  print_header
-  print(students)
-  print_footer(students)
+  show_students
   when "9"
   exit  
   else
   "Sorry I do not understand"
   end
-  
 end
 
 
- 
+def interactive_menu 
+  students = [] 
+  loop do 
+  menu  
+  process
+  end
 end
+
+interactive_menu
  
- interactive_menu
- #students = input_students
- #print_header
- #print(students)
- #print_footer(students)
-
-
